169255dfccadcde26cde837f5b950e3c282e93e9
duytm <duytm2599@gmail.com>
2024-01-25 10:28:20 +0700

    search bar

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,13 +1,28 @@
-import { Box } from "@mui/material";
+import { Box, Container, TextField } from "@mui/material";
 import Header from "../../components/Header/header";
 import Footer from "../../components/Footer/footer";
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
 
 const HomePage = () => {
   return (
     <Box>
       <Header />
-      <Box component="main" sx={{ marginTop: "70px" }}>
-        lorem2000
+      <Box component="main" sx={{ marginTop: "70px", height: "500px" }}>
+        <Container disableGutters>
+          <TextField
+            label="Search"
+            variant="outlined"
+            fullWidth
+            InputProps={{
+              startAdornment: (
+                <IconButton type="submit" aria-label="search">
+                  <SearchIcon />
+                </IconButton>
+              ),
+            }}
+          />
+        </Container>
       </Box>
       <Footer />
     </Box>
